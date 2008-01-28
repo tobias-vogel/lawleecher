eb5f328006e879b14809df18ead1e37db444a30d
tobias.vogel <tobias.vogel@a4c7ed19-0133-43c0-a1b6-ae331e006e1c>
2008-01-28 15:45:52 +0000

    geht nun schon etwas
    git-svn-id: svn+ssh://code/svn@7 a4c7ed19-0133-43c0-a1b6-ae331e006e1c

--- demo.rb
+++ demo.rb
@@ -1,10 +1,34 @@
-require 'tk'
-require 'rubyscript2exe'
-root = TkRoot.new { title "Ex1" }
-TkLabel.new(root) do
-  text 'Hello, World!'
-  pack { padx 15 ; pady 15; side 'left' }
+def exportieren
+    20.times {puts "exportieren..."}
 end
+
+
+require 'tk'
+#require 'rubyscript2exe'
+
+mainWindow = TkRoot.new do
+    title 'Law Leecher'
+end
+
+label = TkLabel.new(mainWindow) do
+    text 'Hello, World!'
+    pack {padx 15 ; pady 15; side 'left'}
+end
+
+button = TkButton.new(mainWindow) do
+    text "Export starten"
+    command proc {exportieren}
+end
+button.pack
+
+mainWindow.bind('ButtonRelease-1') {
+    puts "losgelassen"
+}
+
+# button.bind('ButtonClick-1') {
+#     exportieren
+# }
+
 puts 'nanu'
-exit if RUBYSCRIPT2EXE.is_compiling?
+#exit if RUBYSCRIPT2EXE.is_compiling?
 Tk.mainloop
--- http.rb
+++ http.rb
@@ -4,8 +4,11 @@ require 'net/http'
 # declaration of some important parameters of the program
 ################################################################################
 
-#types = ["CNS", "COD", "SYN", "AVC"]
+types = ["CNS", "COD", "SYN", "AVC"]
 types = ["AVC"]
+
+#year, empty string for "all years"
+year = '1998'
 
 numberOfMaxHitsPerPage = 10000 #max on the web front is 99
 
@@ -86,18 +89,22 @@ types.each do |type|
     puts "looking for #{type} laws..."
     # start query for current type
     # todo: key => value, damit mans besser lesen kann
-    response = Net::HTTP.start('ec.europa.eu').post("/prelex/liste_resultats.cfm?CL=en", "doc_typ=&docdos=dos&requete_id=0&clef1=" + type + "&doc_ann=&doc_num=&doc_ext=&clef4=&clef2=2000&clef3=&LNG_TITRE=EN&titre=&titre_boolean=&EVT1=&GROUPE1=&EVT1_DD_1=&EVT1_MM_1=&EVT1_YY_1=&EVT1_DD_2=&EVT1_MM_2=&EVT1_YY_2=&event_boolean=+and+&EVT2=&GROUPE2=&EVT2_DD_1=&EVT2_MM_1=&EVT2_YY_1=&EVT2_DD_2=&EVT2_MM_2=&EVT2_YY_2=&EVT3=&GROUPE3=&EVT3_DD_1=&EVT3_MM_1=&EVT3_YY_1=&EVT3_DD_2=&EVT3_MM_2=&EVT3_YY_2=&TYPE_DOSSIER=&NUM_CELEX_TYPE=&NUM_CELEX_YEAR=&NUM_CELEX_NUM=&BASE_JUR=&DOMAINE1=&domain_boolean=+and+&DOMAINE2=&COLLECT1=&COLLECT1_ROLE=&collect_boolean=+and+&COLLECT2=&COLLECT2_ROLE=&PERSON1=&PERSON1_ROLE=&person_boolean=+and+&PERSON2=&PERSON2_ROLE=&nbr_element=" + numberOfMaxHitsPerPage.to_s + "&first_element=1&type_affichage=1")
+    response = Net::HTTP.start('ec.europa.eu').post('/prelex/liste_resultats.cfm?CL=en', "doc_typ=&docdos=dos&requete_id=0&clef1=#{type}&doc_ann=&doc_num=&doc_ext=&clef4=&clef2=#{year}&clef3=&LNG_TITRE=EN&titre=&titre_boolean=&EVT1=&GROUPE1=&EVT1_DD_1=&EVT1_MM_1=&EVT1_YY_1=&EVT1_DD_2=&EVT1_MM_2=&EVT1_YY_2=&event_boolean=+and+&EVT2=&GROUPE2=&EVT2_DD_1=&EVT2_MM_1=&EVT2_YY_1=&EVT2_DD_2=&EVT2_MM_2=&EVT2_YY_2=&EVT3=&GROUPE3=&EVT3_DD_1=&EVT3_MM_1=&EVT3_YY_1=&EVT3_DD_2=&EVT3_MM_2=&EVT3_YY_2=&TYPE_DOSSIER=&NUM_CELEX_TYPE=&NUM_CELEX_YEAR=&NUM_CELEX_NUM=&BASE_JUR=&DOMAINE1=&domain_boolean=+and+&DOMAINE2=&COLLECT1=&COLLECT1_ROLE=&collect_boolean=+and+&COLLECT2=&COLLECT2_ROLE=&PERSON1=&PERSON1_ROLE=&person_boolean=+and+&PERSON2=&PERSON2_ROLE=&nbr_element=#{numberOfMaxHitsPerPage.to_s}&first_element=1&type_affichage=1")
 
     content = response.body
 
+
+#puts content[content.size-2500..content.size-700]
 
     # check, whether all hits are on the page
     # there are two ways to check it, we use both for safety reasons
 
     # first, compare the last number with the max number (e.g. 46/2110)
     # if it's equal, all hits are on this page, which is good
-    lastEntryOnPage = content[/\d{1,5}\/\d{1,5}(?=<\/div>\n\t\t<\/TD>\n\t<\/TR>\n\t\n\t\t<TR bgcolor=\"#ffffcc\">\n        \t<TD colspan=\"2\" VALIGN=\"top\">\n        \t<FONT CLASS=\"texte\"> Proposal for a COUNCIL DECISION on the accession of the European Community to Regulation 109 of the United Nations Economic Commission for Europe concerning the approval for the production of retreaded pneumatic tyres for commercial vehicles and their trailers<\/FONT>\n        \t<\/TD>\n\t\t<\/TR>\n\t\n\n\n<\/table>\n\n\n\n<center>\n<TABLE border=0 cellpadding=0 cellspacing=0>\n<tr align=\"center\">\n\t\n\n\n\t\n\t\n<\/tr>\n<\/table>\n<\/center>\n\n\n\r\n\r\n\r\n\r\n\r\n\r\n\t\t\t\t\r\n\t\t\t\t<!-- BOTTOM NAVIGATION BAR)/]
 
+    lastEntryOnPage = content[/\d{1,5}\/\d{1,5}(?=<\/div>\s*<\/TD>\s*<\/TR>\s*<TR bgcolor=\"#(ffffcc|ffffff)\">\s*<TD colspan=\"2\" VALIGN=\"top\">\s*<FONT CLASS=\"texte\">.*<\/FONT>\s*<\/TD>\s*<\/TR>\s*<\/table>\s*<center>\s*<TABLE border=0 cellpadding=0 cellspacing=0>\s*<tr align=\"center\">\s*<\/tr>\s*<\/table>\s*<\/center>\s*<!-- BOTTOM NAVIGATION BAR)/]
+
+#exit 0
     lastEntry, maxEntries = lastEntryOnPage.split("/", 2)
 
     #TODO:EXCEOTION werfen
@@ -134,13 +141,16 @@ puts "#{numberOfLaws} laws found in total"
 # STEP 2: crawl each page
 ################################################################################
 
-        #array containing all law information
+#array containing all law information
 results = Array.new
 
 
 # for each lawID, submit HTTP GET request for fetching out the information of interest
 currentLaw = 1
 lawIDs.each do |lawID|
+
+    startTime = Time.now
+
     puts "retrieving law ##{lawID} (#{currentLaw}/#{numberOfLaws})"
     response = fetch("http://ec.europa.eu/prelex/detail_dossier_real.cfm?CL=en&DosId=#{lawID}")
     content = response.body
@@ -156,16 +166,16 @@ lawIDs.each do |lawID|
     # find out the value for "fields of activity"
     fieldsOfActivity = content[/Fields of activity:<\/font>\s*<\/center>\s*<\/td>\s*<td BGCOLOR="#EEEEEE">\s*<font face="Arial,Helvetica" size=-2>\s*.*?(?=<\/tr>)/m]
     fieldsOfActivity.gsub!(/Fields of activity:<\/font>\s*<\/center>\s*<\/td>\s*<td BGCOLOR="#EEEEEE">\s*<font face="Arial,Helvetica" size=-2>/, '')
-    fieldsOfActivity.gsub!(/<br>\s*<\/font>\s*<\/td>/, '')
+    #fieldsOfActivity.gsub!(/<br>\s*<\/font>\s*<\/td>/, '')
     fieldsOfActivity = removeDust(fieldsOfActivity)
-    arrayEntry["Fields of activity"] = fieldsOfActivity
+    arrayEntry['Fields of activity'] = fieldsOfActivity
 
 
     # find out the value for "legal basis"
     legalBasis = content[/Legal basis:\s*<\/font>\s*<\/center>\s*<\/td>\s*<td BGCOLOR="#FFFFFF">\s*<font face="Arial,Helvetica" size=-2>.*?(?=<\/tr>)/m]
     legalBasis.gsub!(/Legal basis:\s*<\/font>\s*<\/center>\s*<\/td>\s*<td BGCOLOR="#FFFFFF">\s*<font face="Arial,Helvetica" size=-2>/, '')
     legalBasis = removeDust(legalBasis)
-    arrayEntry["Legal basis"] = legalBasis
+    arrayEntry['Legal basis'] = legalBasis
 
 
     # find out the value for "procedures"
@@ -173,7 +183,7 @@ lawIDs.each do |lawID|
     procedures.gsub!(/Procedures:<\/font>\s*<\/center>\s*<\/td>\s*<td BGCOLOR="#EEEEEE">\s*<font face="Arial,Helvetica" size=-2>/, '')
     # convert all \t resp. \r\n into blanks
     procedures = removeDust(procedures)
-    arrayEntry["Procedures"] = procedures
+    arrayEntry['Procedures'] = procedures
 
 
     # find out the value for "type of file"
@@ -181,20 +191,30 @@ lawIDs.each do |lawID|
     typeOfFile.gsub!(/Type of file:<\/font>\s*<\/center>\s*<\/td>\s*<td BGCOLOR="#FFFFFF">\s*<font face="Arial,Helvetica" size=-2>/, '')
     # convert all \t resp. \r\n into blanks
     typeOfFile = removeDust(typeOfFile)
-    arrayEntry["Type of File"] = typeOfFile
+    arrayEntry['Type of File'] = typeOfFile
 
 
     # find out the value for "primarily responsible"
     primarilyResponsible = content[/Primarily responsible<\/font><\/font><\/td>\s*<td VALIGN=TOP><font face="Arial"><font size=-2>.*?(?=<\/tr>)/m]
-    primarilyResponsible.gsub!(/Primarily responsible<\/font><\/font><\/td>\s*<td VALIGN=TOP><font face="Arial"><font size=-2>/, '')
-    # convert all \t resp. \r\n into blanks
-    primarilyResponsible = removeDust(primarilyResponsible)
-    arrayEntry["Primarily Responsible"] = primarilyResponsible
+    # primarily responsible may be empty
+    if primarilyResponsible == nil
+        primarilyResponsible == ''
+    else
+        primarilyResponsible.gsub!(/Primarily responsible<\/font><\/font><\/td>\s*<td VALIGN=TOP><font face="Arial"><font size=-2>/, '')
+        # convert all \t resp. \r\n into blanks
+        primarilyResponsible = removeDust(primarilyResponsible)
+    end
+    arrayEntry['Primarily Responsible'] = primarilyResponsible
+
+
+    endTime = Time.now
+
+    arrayEntry['Duration'] = endTime - startTime
 
 
 #        arrayEntry.each {|i, j| puts "#{i} => #{j}"; puts}
 
-    #add the law, processed above
+    #add the law processed above
     results << arrayEntry
 
     currentLaw += 1
@@ -223,36 +243,21 @@ results.each do |law|
     file.puts temp.join(separator)
 end
 
-#file.puts law.values.join(separator)}
 
 
 puts "#{results.size} laws written into #{fileName}"
+
+
+
+sum = 0
+results.each {|i| sum += i['Duration']}
+puts "total duration: #{sum / 60} minutes"
+averageDuration = sum / results.size
+puts "average duration per law: #{averageDuration} seconds"
+
+
 exit 0
 
-
-
-
-
-
-
-
-  #response.body.scan() do |title|
-    #puts title
-  #end
-  #puts response.class.inspect
-#   puts "din der scheife"
-  f = File.new("response.html", "w")
-  f.puts response.body
-  f.close
-
-
-#   g = File.new("response.html", "r")
-#   15.times do |i|
-#     puts g.gets
-#   end
-  #read out number of results (e.g. 13
-
-  #response.body.
-
-
-
+f = File.new("response.html", "w")
+f.puts response.body
+f.close
