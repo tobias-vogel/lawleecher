bbd9829e67abc219149d63200983b29b0aba82c4
tobias.vogel <tobias.vogel@a4c7ed19-0133-43c0-a1b6-ae331e006e1c>
2008-02-23 15:19:25 +0000

    tester geändert, vermutlich nur die ids
    git-svn-id: svn+ssh://code/svn@29 a4c7ed19-0133-43c0-a1b6-ae331e006e1c

--- Law Leecher/test/tester.rb
+++ Law Leecher/test/tester.rb
@@ -17,5 +17,5 @@ core = Core.new
 core.addGuiPointer(GUI.new(core))
 fetcher = Fetcher.new(core)
 
-results, processStepNames, thereHaveBeenErrors = fetcher.retrieveLawContents([193324, 196330])
+results, processStepNames, thereHaveBeenErrors = fetcher.retrieveLawContents([193636])
 Saver.new(core).save(results, processStepNames, "c:\\export.csv")
